fb3267a9d2bc20d288f16daae6f637ef3873ec44
jkgrim <j.grimshaw37@gmail.com>
2023-02-17 20:06:57 -0700

    adds functionality to solve the sudoku, reset the board, and have mulitple checks

--- src/App.js
+++ src/App.js
@@ -29,16 +29,111 @@ function App() {
     setSudokuArr(grid);
   }
 
+  function compareSudokus(currentSudoku, solvedSudoku) {
+    let res = {
+      isComplete: true,
+      isSolvable: true,
+    };
+    for (var i = 0; i < 9; i++) {
+      for (var j = 0; j < 9; j++) {
+        if (currentSudoku[i][j] !== solvedSudoku[i][j]) {
+          if (currentSudoku[i][j] !== -1) {
+            res.isSolvable = false;
+          }
+          res.isComplete = false;
+        }
+      }
+    }
+    return res;
+  }
+
   function checkSudoku() {
-    // Checks sudoku for user input and pops up an alert whether or not it is completed properly
+    let sudoku = getDeepCopy(puzzle);
+    solver(sudoku);
+    let compare = compareSudokus(sudokuArr, sudoku);
+    if (compare.isComplete) {
+      alert("Congratulations! You have solved the Sudoku!");
+    } else if (compare.isSolvable) {
+      alert("Keep going! You're on the right track!");
+    } else {
+      alert("You've messed up somewhere... Try something else...");
+    }
+  }
+
+  function checkRow(grid, row, num) {
+    return grid[row].indexOf(num) === -1;
+  }
+
+  function checkCol(grid, col, num) {
+    return grid.map((row) => row[col]).indexOf(num) === -1;
+  }
+
+  function checkBox(grid, row, col, num) {
+    let boxArr = [],
+      rowStart = row - (row % 3),
+      colStart = col - (col % 3);
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        boxArr.push(grid[rowStart + i][colStart + j]);
+      }
+    }
+
+    return boxArr.indexOf(num) === -1;
+  }
+
+  function checkValid(grid, row, col, num) {
+    if (
+      checkRow(grid, row, num) &&
+      checkCol(grid, col, num) &&
+      checkBox(grid, row, col, num)
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  function getNext(row, col) {
+    return col !== 8 ? [row, col + 1] : row !== 8 ? [row + 1, 0] : [0, 0];
+  }
+
+  function solver(grid, row = 0, col = 0) {
+    if (grid[row][col] !== -1) {
+      let isLast = row >= 8 && col >= 8;
+      if (!isLast) {
+        let [newRow, newCol] = getNext(row, col);
+        return solver(grid, newRow, newCol);
+      }
+    }
+
+    for (let num = 1; num <= 9; num++) {
+      if (checkValid(grid, row, col, num)) {
+        grid[row][col] = num;
+
+        let [newRow, newCol] = getNext(row, col);
+
+        if (!newRow && !newCol) {
+          return true;
+        }
+
+        if (solver(grid, newRow, newCol)) {
+          return true;
+        }
+      }
+    }
+
+    grid[row][col] = -1;
+    return false;
   }
 
   function solveSudoku() {
-    // fills in all empty answers and corrects incorrect user-input answers
+    let sudoku = getDeepCopy(puzzle);
+    solver(sudoku);
+    setSudokuArr(sudoku);
   }
 
   function resetSudoku() {
-    // clears board
+    let sudoku = getDeepCopy(puzzle);
+    setSudokuArr(sudoku);
   }
 
   return (
@@ -78,13 +173,13 @@ function App() {
           </tbody>
         </table>
         <div className="button-container">
-          <button className="check-button" onClick={checkSudoku()}>
+          <button className="check-button" onClick={checkSudoku}>
             Check
           </button>
-          <button className="solve-button" onClick={solveSudoku()}>
+          <button className="solve-button" onClick={solveSudoku}>
             Solve
           </button>
-          <button className="reset-button" onClick={resetSudoku()}>
+          <button className="reset-button" onClick={resetSudoku}>
             Reset
           </button>
         </div>
